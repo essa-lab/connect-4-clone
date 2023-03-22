e3b2c2e4ca34a54bc7b357954a4a6505486b835b
essa-lab <essaarar4@gmail.com>
2023-03-22 18:51:47 +0200

    First Commit

---
+++ src/Components/GameBoard.js
@@ -0,0 +1,35 @@
+import React from "react";
+import GameCircle from "./GameCircle";
+const GameBoard = () =>{
+    return (
+    <div>
+        <GameCircle id={1} color="red">
+            <span style={{color:'Red'}}>Red</span>
+        </GameCircle>
+        <GameCircle id={2} color="blue">
+        <span style={{color:'blue'}} >Blue</span>
+        </GameCircle>
+        <GameCircle id={3} color="red">
+        <span style={{color:'Red'}} >Red</span>
+        </GameCircle>
+        <GameCircle id={4} color="blue">
+        <span style={{color:'blue'}} >Blue</span>
+        </GameCircle>
+        <GameCircle id={5} color="red">
+        <span style={{color:'Red'}} >Red</span>
+        </GameCircle>
+        <GameCircle id={6} color="blue">
+        <span style={{color:'blue'}} >Blue</span>
+        </GameCircle>
+        <GameCircle id={7} color="red">
+        <span style={{color:'Red'}} >Red</span>
+        </GameCircle>
+        <GameCircle id={8} color="blue">
+        <span style={{color:'blue'}} >Blue</span>
+        </GameCircle>
+
+    </div>
+        )
+
+}
+export default GameBoard;
---
+++ src/Components/GameCircle.js
@@ -0,0 +1,18 @@
+import React from "react";
+
+const onClick = (id) => {
+
+    alert('on click' + id);
+}
+
+const GameCircle = ({id,color, children}) => {
+    console.log(id);
+    return (
+        <div style ={{backgroundColor : color}}onClick={()=>onClick(id)}>
+            {children}
+        </div>
+    )
+    
+}
+
+export default GameCircle
